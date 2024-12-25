c5cc36466c732c889674aa4e61610ba29fdd1fba
MRSA <sanggisda@gmail.com>
2024-12-25 13:40:47 +0700

    add: seeder user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Seeder;
+// use illuminate\Database\Eloquent\Factories\Factory;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        Factory::factoryForModel(User::class)->count(50)->create();
+    }
+}
